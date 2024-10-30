ddf5ae8342f02847f2d81dcf430dbe68ae92219f
Bartłomiej Więcek <bartlomiej.wiecek@vp.pl>
2024-10-30 09:02:53 +0100

    implemented app functionalities

--- mobilna/mobilna/MainPage.xaml.cs
+++ mobilna/mobilna/MainPage.xaml.cs
@@ -10,9 +10,33 @@ namespace mobilna
 {
     public partial class MainPage : ContentPage
     {
+        List<string> items = new List<string>() { "Pies", "Kot", "Świnka morska" };
         public MainPage()
         {
             InitializeComponent();
+            list.ItemsSource = items;
+            list.HeightRequest = 40 * items.Count + 10;
+        }
+
+        private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (list.SelectedItem)
+            {
+                case "Pies":
+                    slider.Maximum = 18;
+                    break;
+                case "Kot":
+                    slider.Maximum = 20;
+                    break;
+                case "Świnka morska":
+                    slider.Maximum = 9;
+                    break;
+            }
+        }
+
+        private void DisplayData(object sender, EventArgs e)
+        {
+            DisplayAlert("Wprowadzone dane", owner.Text + ", " + list.SelectedItem + ", " + (int)slider.Value + ", " + purpose.Text + ", " + time.Time, "OK");
         }
     }
 }
